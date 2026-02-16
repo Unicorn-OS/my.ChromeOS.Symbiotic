a22b2c7023d2317a3545fdc430004c771e52be08
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-16 16:59:34 +0000

    Create Recovery.Image.sh

---
+++ Hw/Lenovo/IdeaPad 3/Chrome 14M836/Recovery.Image.sh
@@ -0,0 +1,4 @@
+# Source: https://github.com/Unicorn-OS/ChromeOS/tree/main/.graph/Recovery%20image
+#name=IdeaPad 3 Chromebook
+#hwidmatch=^FENNEL14-.*
+wget -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16503.60.0_jacuzzi_recovery_stable-channel_JacuzziMPKeys-v19.bin.zip
