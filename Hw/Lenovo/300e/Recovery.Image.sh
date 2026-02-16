b900e42750d9f23139368da309d4dad24b4fff58
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-16 17:00:40 +0000

    Update Recovery.Image.sh

--- Hw/Lenovo/300e/Recovery.Image.sh
+++ Hw/Lenovo/300e/Recovery.Image.sh
@@ -1,2 +1,5 @@
 # Source: https://github.com/Unicorn-OS/ChromeOS/tree/main/.graph/Recovery%20image
+#name=Lenovo 300e Chromebook 2nd Gen MTK
+#hwidmatch=^HANA ([A-Z0-9])+-([A-Z0-9])+-[A-Z0-9][A-Z0-9]I-([A-Z0-9\-])+
+
 wget -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16267.66.0_hana_recovery_stable-channel_HanaMPKeys-v13.bin.zip
