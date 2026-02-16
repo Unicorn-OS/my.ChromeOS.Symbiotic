1bc705def08074faf3c5c4d376fd471f0ae30062
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-16 16:56:23 +0000

    Update Recovery.Image.sh

--- Hw/Lenovo/300e/Recovery.Image.sh
+++ Hw/Lenovo/300e/Recovery.Image.sh
@@ -1,1 +1,2 @@
+# Source: https://github.com/Unicorn-OS/ChromeOS/tree/main/.graph/Recovery%20image
 wget -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16267.66.0_hana_recovery_stable-channel_HanaMPKeys-v13.bin.zip
