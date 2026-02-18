2551bda64b43195f0704acaea727f4b42006fccd
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-18 15:11:24 +0000

    Update dl.Recovery.Image.sh

--- Hw/Lenovo/all:/dl.Recovery.Image.sh
+++ Hw/Lenovo/all:/dl.Recovery.Image.sh
@@ -1,11 +1,14 @@
 # Source: https://github.com/Unicorn-OS/ChromeOS/tree/main/.graph/Recovery%20image
 args=¨--no-check-certificate¨
 
-#
-cd 300e
+id=300e
+#name=Lenovo 300e Chromebook 2nd Gen MTK
+#hwidmatch=^HANA ([A-Z0-9])+-([A-Z0-9])+-[A-Z0-9][A-Z0-9]I-([A-Z0-9\-])+
+mkdir -p $id; cd $id
 wget $args -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16267.66.0_hana_recovery_stable-channel_HanaMPKeys-v13.bin.zip
 
-cd ..; cd IdeaPad3
+id=IdeaPad3
 #name=IdeaPad 3 Chromebook
 #hwidmatch=^FENNEL14-.*
+cd ..; mkdir -p $id; cd $id
 wget $args -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16503.60.0_jacuzzi_recovery_stable-channel_JacuzziMPKeys-v19.bin.zip
