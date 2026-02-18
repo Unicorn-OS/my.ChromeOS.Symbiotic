58e880822499ac49efcd2304811c77f0bb6b7cc8
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2026-02-18 15:19:25 +0000

    Update dl.Recovery.Image.sh

--- Hw/Lenovo/all:/dl.Recovery.Image.sh
+++ Hw/Lenovo/all:/dl.Recovery.Image.sh
@@ -5,10 +5,10 @@ id=300e
 #name=Lenovo 300e Chromebook 2nd Gen MTK
 #hwidmatch=^HANA ([A-Z0-9])+-([A-Z0-9])+-[A-Z0-9][A-Z0-9]I-([A-Z0-9\-])+
 mkdir -p $id; cd $id
-wget $args -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16267.66.0_hana_recovery_stable-channel_HanaMPKeys-v13.bin.zip
+wget -c $args https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16267.66.0_hana_recovery_stable-channel_HanaMPKeys-v13.bin.zip
 
 id=IdeaPad3
 #name=IdeaPad 3 Chromebook
 #hwidmatch=^FENNEL14-.*
 cd ..; mkdir -p $id; cd $id
-wget $args -c https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16503.60.0_jacuzzi_recovery_stable-channel_JacuzziMPKeys-v19.bin.zip
+wget -c $args https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_16503.60.0_jacuzzi_recovery_stable-channel_JacuzziMPKeys-v19.bin.zip
